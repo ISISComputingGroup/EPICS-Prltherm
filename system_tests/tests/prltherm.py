bb73910ab1cfff25ad2e4d23b50ffe6eb25af7ae
DavidKeymer <david.keymer@stfc.ac.uk>
2024-03-01 17:21:50 +0000

    Added PV checking test

--- system_tests/tests/prltherm.py
+++ system_tests/tests/prltherm.py
@@ -1,10 +1,8 @@
 import unittest
 
 from utils.channel_access import ChannelAccess
-from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc
-
+from utils.ioc_launcher import get_default_ioc_dir, ProcServLauncher, IOCRegister
 
 DEVICE_PREFIX = "PRLTHERM_01"
 
@@ -14,7 +12,8 @@ IOCS = [
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("PRLTHERM"),
         "macros": {},
-        "pv_for_existence": "TEMP01"
+        "pv_for_existence_base_name": "TEMP",
+        "ioc_launcher_class": ProcServLauncher,
     },
 ]
 
@@ -26,9 +25,13 @@ class PrlthermTests(unittest.TestCase):
     """
     Tests for the Prltherm IOC (PEARL Thermometry device using DAQmx).
     """
+   
     def setUp(self):
-        self._lewis, self._ioc = get_running_lewis_and_ioc("Prltherm", DEVICE_PREFIX)
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+        self._ioc = IOCRegister.get_running(DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_wait_time=0.0)
+        self.ca.assert_that_pv_exists("DISABLE", timeout=30)
 
-    def test_WHEN_ioc_is_started_THEN_PV_exists(self):
-        self.ca.assert_that_pv_exists(IOCS[0]['pv_for_existence'], timeout=30)
+    def test_WHEN_ioc_is_started_THEN_PVs_for_all_channels_exist(self):
+        for i in range(1,17,1):
+            pv_to_check = IOCS[0]['pv_for_existence_base_name']+f"{i:02}"
+            self.ca.assert_that_pv_exists(pv_to_check, timeout=30)
