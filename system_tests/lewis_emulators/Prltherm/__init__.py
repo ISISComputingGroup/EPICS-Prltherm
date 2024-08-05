45aa52dd4f19a9274b574960e3c5ebcb2ca03c8e
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-08-05 14:04:28 +0100

    run ruff format

--- system_tests/lewis_emulators/Prltherm/__init__.py
+++ system_tests/lewis_emulators/Prltherm/__init__.py
@@ -2,4 +2,4 @@ from ..lewis_versions import LEWIS_LATEST
 from .device import SimulatedPrltherm
 
 framework_version = LEWIS_LATEST
-__all__ = ['SimulatedPrltherm']
+__all__ = ["SimulatedPrltherm"]
--- system_tests/tests/prltherm.py
+++ system_tests/tests/prltherm.py
@@ -26,13 +26,13 @@ class PrlthermTests(unittest.TestCase):
     """
     Tests for the Prltherm IOC (PEARL Thermometry device using DAQmx).
     """
-   
+
     def setUp(self):
         self._ioc = IOCRegister.get_running(DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_wait_time=0.0)
         self.ca.assert_that_pv_exists("DISABLE", timeout=30)
 
     def test_WHEN_ioc_is_started_THEN_PVs_for_all_channels_exist(self):
-        for i in range(1,IOCS[0]['number_of_channels']+1,1):
-            pv_to_check = IOCS[0]['pv_for_existence_base_name']+f"{i:02}"
+        for i in range(1, IOCS[0]["number_of_channels"] + 1, 1):
+            pv_to_check = IOCS[0]["pv_for_existence_base_name"] + f"{i:02}"
             self.ca.assert_that_pv_exists(pv_to_check, timeout=30)
